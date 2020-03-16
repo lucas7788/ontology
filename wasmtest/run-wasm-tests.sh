d28ffbcccb2fa3f8e9e5c3dd341ce95f1b19c877
steven <chenglin@ont.com>
2020-03-16 15:30:17 +0800

    cli tool

--- wasmtest/run-wasm-tests.sh
+++ wasmtest/run-wasm-tests.sh
@@ -4,8 +4,8 @@ set -x
 
 # install build tools
 if ! which clang-9 ; then
-	wget releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz > /dev/null 2>&1
-	tar xf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz > /dev/null 2>&1
+	wget releases.llvm.org/9.0.0/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+	tar xf clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
 	export PATH="$(pwd)/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04/bin":$PATH
 fi
 
@@ -16,14 +16,15 @@ fi
 rustup target add wasm32-unknown-unknown
 which ontio-wasm-build || cargo install --git=https://github.com/ontio/ontio-wasm-build
 
+contractdir="testwasmdata"
 # build rust wasm contracts
-mkdir -p testwasmdata
+mkdir -p $contractdir
 cd contracts-rust && bash travis.build.sh && cd ../
 
 cd contracts-cplus && bash travis.build.bash && cd ../
 
 # verify and optimize wasm contract
-for wasm in testwasmdata/*.wasm ; do
+for wasm in $contractdir/*.wasm ; do
 	ontio-wasm-build $wasm $wasm
 done
 
